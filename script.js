eb5ce9f01f54146c1d9b98e2cd6559324199d296
Julie Chu <julieychu@gmail.com>
2022-01-04 01:05:04 -0600

    add quiz array variable

--- script.js
+++ script.js
@@ -10,9 +10,35 @@ var initials = "";
 var timer;
 var timerCount;
 
-// Arrays used to content on the screen
-// var questions = [];
-// var choices = [];
+// Arrays used for quiz content on the screen
+// var questions = [{
+//   question: "Commonly used data types DO NOT include:",
+//   choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+//   correctAnswer: 2
+// }, {
+//   question: "The condition in an if / else statement is enclosed within _____.",
+//   choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+//   correctAnswer: 2
+// }, {
+//   question: "Arrays in JavaScript can be used to store ____.",
+//   choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+//   correctAnswer: 3
+// }, {
+//   question: "String values must be enclosed within _____ when being assigned to variables.",
+//   choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+//   correctAnswer: 2
+// }, {
+//   question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//   choices: ["1. JavaScript", "2. terminal / bash", "3. for loops", "4. console log"],
+//   correctAnswer: 3
+// }];
+
+// Need a var or function with initial content?
+  // Centered <h1> Coding Quiz Challenge
+  // Centered <p> Try to answer the following code-related questions within the time limit. 
+  // Keep in mind that incorrect answers will penalize your score time by ten seconds!
+  // Centered button “Start Quiz” 
+    // when clicked, start timer & replace above text with quiz content
 
 // The init function is called when the page loads 
 function init() {
